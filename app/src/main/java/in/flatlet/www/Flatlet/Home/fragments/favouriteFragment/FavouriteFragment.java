de84f17d7bde038a237ce51c2d58931ecba0f0a4
deepu842 <amandeepsharma842@gmail.com>
2017-08-17 16:59:15 +0530

    checking 1

--- app/src/main/java/in/flatlet/www/Flatlet/Home/fragments/favouriteFragment/FavouriteFragment.java
+++ app/src/main/java/in/flatlet/www/Flatlet/Home/fragments/favouriteFragment/FavouriteFragment.java
@@ -38,9 +38,10 @@ public class FavouriteFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         db = new FeedReaderDbHelper(getContext()).getWritableDatabase();
 
-        Log.i(TAG, "onActivityCreated: SQLite Object Created successfully");
+        Log.i(TAG, "onActivityCreated: SQLite Object is Created successfully");
 
 
 
